283ea1bdd456e44e27b37a0dbea24343144ec197
Henry Martínez <henrymartinezchable@live.com>
2023-05-15 22:26:28 -0600

    Initialize demo-migrate-v3

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+const uswds = require('@uswds/compile');
+
+uswds.settings.version = 2;
+uswds.paths.dist.theme = './_theme';
+uswds.paths.src.projectSass = './assets/css';
+
+exports.init = uswds.init;
+exports.compile = uswds.compile;
+exports.watch = uswds.watch;
